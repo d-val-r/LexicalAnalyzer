1f2d9dddb9f88b1176d05ea76ac681510c5145fc
d-val-r <dvrudenya@noctrl.edu>
2021-02-27 17:53:38 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+# include <iostream>
+# include <vector>
+# include <map>
+using namespace std;
+
+class LexAnalyzer
+{
+private:
+	vector<string> lexemes;
+	vector<string> tokens;
+	map<string, string> tokenmap;
+public:
+	LexAnalyzer(istream& infile)
+	{
+		string key;
+		string value;		
+		
+		infile >> key >> value;
+		while (!infile.eof())
+		{
+			tokenmap[key] = value;
+			infile >> key >> value;
+		}	
+
+	}
+};
+
+
+int main()
+{
+	return 0;
+}
