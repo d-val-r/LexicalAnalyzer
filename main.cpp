0edcfc73be53531b39ba30b5ee3df7457668748a
d-val-r <dvrudenya@noctrl.edu>
2021-03-01 23:17:43 -0600

    almost finished fileScan()

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ private:
 	vector<string> lexemes;
 	vector<string> tokens;
 	map<string, string> tokenmap;
+
+	
 public:
 	LexAnalyzer(istream& infile)
 	{
@@ -24,6 +26,42 @@ public:
 		}	
 
 	}
+
+	string scanFile(istream& infile)
+	{
+		string message = "";
+		string buffer;
+		int start_quote = -1;
+	        int end_quote = -1;	
+		int i = 0;
+		while (!infile.eof())
+		{
+			getline(infile, buffer);
+			while (buffer[i] != ';' && i < buffer.length())	
+			{
+				if (buffer[i] == '"' && start_quote == -1)
+					start_quote = i;
+				else if (buffer[i] == '"' && start_quote != -1)
+					end_quote = i;
+				message += buffer[i];
+				i++;
+			}
+			
+			// the thing causing the program to not read a semicolon inside
+			// quotes improperly is most likely here
+			if (start_quote != -1 && end_quote == -1 || i >= buffer.length())
+				continue;
+			else
+			{
+				start_quote = end_quote = -1;	
+				i = 0;
+			}
+		}		
+		cout << start_quote << endl;
+		cout << end_quote << endl;
+		cout << i << endl;
+		return message;
+	}
 	
 };
 
@@ -31,8 +69,23 @@ public:
 int main()
 {
 	ifstream file("tokenlexemedata.txt");
-	l.read();
-	
+
+	string source_code;
+	cout << "Please enter a source code file name, with extension: ";
+	cin >> source_code;
+
+	ifstream source(source_code);
+
+	LexAnalyzer l(file);
+
+	string x = l.scanFile(source);
+	cout << x << endl;
+
+	// ARE RANDOM STRINGS CONSIDERED VALID SYNTAX?
+	// ex:
+	// 	main()
+	// 	"hello"
+
 
 	return 0;
 }
