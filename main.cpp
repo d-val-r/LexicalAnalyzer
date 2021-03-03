0a94939417e74f39d446b02a05de51a3b5fdbf1b
d-val-r <dvrudenya@noctrl.edu>
2021-03-02 22:13:46 -0600

    rerouted output from console to file

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ public:
 	}
 	// do not forget to update statements to output to a file,
 	// not to console
-	void scanFile(istream& infile)
+	void scanFile(istream& infile, ostream& outfile)
 	{
 		string line;
 		infile >> line;
@@ -91,119 +91,119 @@ public:
 			
 			if(line == "begin")
 			{
-				cout << tokenmap["t_begin"] << " : " << line << endl;
+				outfile << tokenmap["t_begin"] << " : " << line << endl;
 			}
 			else if(line == "end")
 			{
-				cout << tokenmap["t_end"] << " : " << line << endl;
+				outfile << tokenmap["t_end"] << " : " << line << endl;
 			}else if(line == "else")
 			{
-				cout << tokenmap["t_else"] << " : " << line << endl;
+				outfile << tokenmap["t_else"] << " : " << line << endl;
 			}else if(line == "if")
 			{
-				cout << tokenmap["t_if"] << " : " << line << endl;
+				outfile << tokenmap["t_if"] << " : " << line << endl;
 			}else if(line == "input")
 			{
-				cout << tokenmap["t_input"] << " : " << line << endl;
+				outfile << tokenmap["t_input"] << " : " << line << endl;
 			}else if(line == "integer")
 			{
-				cout << tokenmap["t_integer"] << " : " << line << endl;
+				outfile << tokenmap["t_integer"] << " : " << line << endl;
 				infile >> line;
 				line = identifier_handler(line, infile);	
 				if (line == "invalid variable name")
 					error = true;	
 				 
-				cout << "t_id" << " : " << line << endl;
+				outfile << "t_id" << " : " << line << endl;
 			}else if(line == "string")
 				// is string a keyword for variable declaration or
 				// is it meant to identiy string literals?
 			{
-				cout << tokenmap["t_string"] << " : " << line << endl;
+				outfile << tokenmap["t_string"] << " : " << line << endl;
 				infile >> line;
 				line = identifier_handler(line, infile);	
 				if (line == "invalid variable name")
 					error = true;	
-				cout << "t_id" << " : " << line << endl; 
+				outfile << "t_id" << " : " << line << endl; 
 			}else if(line == "loop")
 			{
-				cout << tokenmap["t_loop"] << " : " << line << endl;
+				outfile << tokenmap["t_loop"] << " : " << line << endl;
 			}else if(line == "main()")
 			{
-				cout << tokenmap["t_main"] << " : " << "main" << endl;
-				cout << tokenmap["s_lparen"] << " : " << "(" << endl;
-				cout << tokenmap["s_rparen"] << " : " << ")" << endl;
+				outfile << tokenmap["t_main"] << " : " << "main" << endl;
+				outfile << tokenmap["s_lparen"] << " : " << "(" << endl;
+				outfile << tokenmap["s_rparen"] << " : " << ")" << endl;
 			}else if(line == "main")
 			{
-				cout << tokenmap["t_main"] << " : " << line << endl;
+				outfile << tokenmap["t_main"] << " : " << line << endl;
 			}else if(line == "output")
 			{
-				cout << tokenmap["t_output"] << " : " << line << endl;
+				outfile << tokenmap["t_output"] << " : " << line << endl;
 			}else if(line == "then")
 			{
-				cout << tokenmap["t_then"] << " : " << line << endl;
+				outfile << tokenmap["t_then"] << " : " << line << endl;
 			}else if(line == "var")
 			{
-				cout << tokenmap["t_var"] << " : " << line << endl;
+				outfile << tokenmap["t_var"] << " : " << line << endl;
 			}else if(line == "while")
 			{
-				cout << tokenmap["t_while"] << " : " << line << endl;
+				outfile << tokenmap["t_while"] << " : " << line << endl;
 			}else if (line == tokenmap["s_assign"])
 			{
-				cout << tokenmap["s_assign"] << " : " << line << endl;
+				outfile << tokenmap["s_assign"] << " : " << line << endl;
 			}else if (line == tokenmap["s_comma"])
 			{
-				cout << tokenmap["s_coma"] << " : " << line << endl;
+				outfile << tokenmap["s_coma"] << " : " << line << endl;
 			}else if (line == tokenmap["s_lparen"])
 			{
-				cout << tokenmap["s_lparen"] << " : " << line << endl;
+				outfile << tokenmap["s_lparen"] << " : " << line << endl;
 			}else if (line == tokenmap["s_rparen"])
 			{
-				cout << tokenmap["s_rparen"] << " : " << line << endl;
+				outfile << tokenmap["s_rparen"] << " : " << line << endl;
 			}else if (line == tokenmap["s_semi"])
 			{
-				cout << tokenmap["s_semi"] << " : " << line << endl;
+				outfile << tokenmap["s_semi"] << " : " << line << endl;
 			}else if (line == tokenmap["s_lt"])
 			{
-				cout << tokenmap["s_lt"] << " : " << line << endl;
+				outfile << tokenmap["s_lt"] << " : " << line << endl;
 			}else if (line == tokenmap["s_le"])
 			{
-				cout << tokenmap["s_le"] << " : " << line << endl;
+				outfile << tokenmap["s_le"] << " : " << line << endl;
 			}else if (line == tokenmap["s_gt"])
 			{
-				cout << tokenmap["s_gt"] << " : " << line << endl;
+				outfile << tokenmap["s_gt"] << " : " << line << endl;
 			}else if (line == tokenmap["s_ge"])
 			{
-				cout << tokenmap["s_ge"] << " : " << line << endl;
+				outfile << tokenmap["s_ge"] << " : " << line << endl;
 			}else if (line == tokenmap["s_eq"])
 			{
-				cout << tokenmap["s_eq"] << " : " << line << endl;
+				outfile << tokenmap["s_eq"] << " : " << line << endl;
 			}else if (line == tokenmap["s_ne"])
 			{
-				cout << tokenmap["s_ne"] << " : " << line << endl;
+				outfile << tokenmap["s_ne"] << " : " << line << endl;
 			}else if (line == tokenmap["s_plus"])
 			{
-				cout << tokenmap["s_plus"] << " : " << line << endl;
+				outfile << tokenmap["s_plus"] << " : " << line << endl;
 			}else if (line == tokenmap["s_minus"])
 			{
-				cout << tokenmap["s_minus"] << " : " << line << endl;
+				outfile << tokenmap["s_minus"] << " : " << line << endl;
 			}else if (line == tokenmap["s_mult"])
 			{
-				cout << tokenmap["s_mult"] << " : " << line << endl;
+				outfile << tokenmap["s_mult"] << " : " << line << endl;
 			}else if (line == tokenmap["s_div"])
 			{
-				cout << tokenmap["s_div"] << " : " << line << endl;
+				outfile << tokenmap["s_div"] << " : " << line << endl;
 			}else if (line == tokenmap["s_mod"])
 			{
-				cout << tokenmap["s_mod"] << " : " << line << endl;
+				outfile << tokenmap["s_mod"] << " : " << line << endl;
 			}else if (line == tokenmap["s_and"])
 			{
-				cout << tokenmap["s_and"] << " : " << line << endl;
+				outfile << tokenmap["s_and"] << " : " << line << endl;
 			}else if (line == tokenmap["s_or"])
 			{
-				cout << tokenmap["s_or"] << " : " << line << endl;
+				outfile << tokenmap["s_or"] << " : " << line << endl;
 			}else if (line == tokenmap["s_not"])
 			{
-				cout << tokenmap["s_not"] << " : " << line << endl;
+				outfile << tokenmap["s_not"] << " : " << line << endl;
 			} // include an else portion that will parse a line
 			  // like, say, x5=;, because otherwise, it will read
 			  // it as one line and it won't match anyhthing above
@@ -224,14 +224,20 @@ int main()
 {
 	ifstream file("tokenlexemedata.txt");
 
-	string source_code;
+	string source_name;
 	cout << "Please enter a source code file name, with extension: ";
-	cin >> source_code;
+	cin >> source_name;
 
-	ifstream source(source_code);
+	ifstream source_code(source_name);
+	string out_name;
+	cout << "Please enter an output file name, with extension: ";
+	cin >> out_name;
+
+	ofstream output(out_name);
+
+
 	LexAnalyzer l(file);
-
-	l.scanFile(source);
+	l.scanFile(source_code, output);
 
 
 	// ARE RANDOM STRINGS CONSIDERED VALID SYNTAX?
@@ -239,6 +245,7 @@ int main()
 	// 	main()
 	// 	"hello"
 
+	
 
 	return 0;
 }
